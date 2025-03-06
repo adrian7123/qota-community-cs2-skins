916ff2f11b444976ea534a1303c0c791ac8270bf
Adrian Bueno <32718131+adrian7123@users.noreply.github.com>
2025-03-06 10:53:26 -0300

    idle database timeout

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,13 +3,9 @@ import type { ConnectionOptions } from "mysql2"
 
 const databaseConnectionOptions: ConnectionOptions = {
   uri: import.meta.env.DATABASE_URL,
-  keepAliveInitialDelay: 10000,
-  enableKeepAlive: true,
-  waitForConnections: true,
-  connectionLimit: 5, // Pool com até 5 conexões
-  queueLimit: 0, // Sem limite de fila
-  connectTimeout: 30000, // 30 segundos para conectar
-  idleTimeout: 14400000 // 1 minuto de inatividade
+  maxIdle: 0,
+  idleTimeout: 60000,
+  enableKeepAlive: true
 }
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
